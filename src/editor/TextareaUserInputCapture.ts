cf6e2139428f6a2a8b3fe3ad47293fbf1dfccf3b
JaPNaA <japnaa@japnaa.dev>
2023-06-01 21:34:18 -0400

    Move events.ts into TextareaUserInputCaputre

--- src/editor/TextareaUserInputCapture.ts
+++ src/editor/TextareaUserInputCapture.ts
@@ -1,6 +1,5 @@
 import { Editable } from "./Editable.js";
 import { EditorCursor } from "./EditorCursor.js";
-import { LineOperationEvent, UserInputEvent } from "./events.js";
 import { Elm } from "../japnaaEngine2d/JaPNaAEngine2d.js";
 
 /** A string represents an editable area with text. A number represents uneditable space by <number> spaces. */
@@ -419,4 +418,32 @@ export interface TextareaUserInputCaptureContext {
     above: TextareaUserInputCaptureAreas;
     current: TextareaUserInputCaptureAreas;
     below: TextareaUserInputCaptureAreas;
+}
+export abstract class RejectableEvent {
+    private rejected = false;
+
+    public reject() {
+        this.rejected = true;
+    }
+
+    public isRejected() {
+        return this.rejected;
+    }
+}
+
+export class UserInputEvent extends RejectableEvent {
+    constructor(
+        public readonly added: string,
+        public readonly removed: string,
+        public readonly newContent: string
+    ) { super(); }
+}
+
+export class LineOperationEvent extends RejectableEvent {
+    constructor(
+        /** Is the operation on the current line or next line? */
+        public readonly isNextLine: boolean,
+        /** Is the operation an insertion or deletion? */
+        public readonly isInsert: boolean
+    ) { super(); }
 }
