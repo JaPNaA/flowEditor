2f8d210af254279743c57ea188a062e1e039519b
JaPNaA <japnaa@japnaa.dev>
2023-07-10 19:12:02 -0400

    Implement ChoiceBranchMacro

--- src/plugins/visualNovel/visualNovel.ts
+++ src/plugins/visualNovel/visualNovel.ts
@@ -1,18 +1,21 @@
+import { ControlItem } from "../../FlowRunner.js";
 import { Editable } from "../../editor/Editable.js";
-import { TextareaUserInputCaptureAreas } from "../../editor/TextareaUserInputCapture.js";
-import { InstructionLineView } from "../../editor/instructionLines.js";
+import { InstructionGroupEditor } from "../../editor/InstructionGroupEditor.js";
+import { BranchInstructionLine, Instruction, InstructionLine, InstructionOneLine, OneLineInstruction } from "../../editor/instructionLines.js";
 import { EditorPlugin } from "../EditorPlugin.js";
 
 export default class VisualNovelPlugin implements EditorPlugin {
-    views: InstructionLineView[] = [];
-    keyMappings: { [x: string]: () => InstructionLineView; } = {
-        "s": () => new SayInstructionLine("a", "b")
+    keyMappings: { [x: string]: () => Instruction; } = {
+        "s": () => new InstructionOneLine(new SayInstruction("", "")),
+        "b": () => new ChoiceBranchMacro(["a", "b"])
     };
     executer = new VisualNovelExecuter();
 
-    parse(data: any): InstructionLineView | undefined {
+    parse(data: any): Instruction | undefined {
         if (data.visualNovelCtrl === "say") {
-            return new SayInstructionLine(data.char, data.text);
+            return new InstructionOneLine(new SayInstruction(data.char, data.text));
+        } else if (data.visualNovelCtrl === "choiceBranch") {
+            return new ChoiceBranchMacro(data.choices);
         }
     }
 }
@@ -30,12 +33,13 @@ class VisualNovelExecuter {
     }
 }
 
-class SayInstructionLine extends InstructionLineView {
+class SayInstruction extends InstructionLine implements OneLineInstruction {
     private characterEditable: Editable;
     private textEditable: Editable;
+    public isBranch: boolean = false;
 
     constructor(character: string, text: string) {
-        super("sayInstructionLine");
+        super();
 
         this.setAreas(
             this.characterEditable = this.createEditable(character),
@@ -47,5 +51,119 @@ class SayInstructionLine extends InstructionLineView {
 
     public serialize() {
         return { visualNovelCtrl: "say", char: this.characterEditable.getValue(), text: this.textEditable.getValue() };
+    }
+}
+
+class ChoiceBranchMacro extends Instruction {
+    private choiceLines: ChoiceBranchMacroLineOption[] = [];
+    private branchOffsets: (number | null)[] = [];
+
+    constructor(choices: string[]) {
+        super();
+        this.addLine(new ChoiceBranchMacroLineOpening());
+        for (const choice of choices) {
+            const line = new ChoiceBranchMacroLineOption(choice);
+            this.addLine(line);
+            this.choiceLines.push(line);
+        }
+    }
+
+    public serialize() {
+        return {
+            visualNovelCtrl: "choiceBranch",
+            choices: this.getChoices()
+        };
+    }
+
+    public isBranch(): boolean {
+        return true;
+    }
+
+    public getBranchTargets(): (InstructionGroupEditor | null)[] | null {
+        const targets: (InstructionGroupEditor | null)[] = [];
+        for (const line of this.choiceLines) {
+            targets.push(line.getBranchTarget());
+        }
+        return targets;
+    }
+
+    public setBranchTargets(targets: (InstructionGroupEditor | null)[] | null): void {
+        if (!targets) {
+            for (const line of this.choiceLines) {
+                line.setBranchTarget(null);
+            }
+        } else {
+            for (let i = 0; i < this.choiceLines.length; i++) {
+                this.choiceLines[i].setBranchTarget(targets[i]);
+            }
+        }
+    }
+
+    public setBranchOffsets(offsets: (number | null)[]): void {
+        this.branchOffsets = offsets;
+    }
+
+    public export() {
+        const choices = this.getChoices();
+        const output: ControlItem[] = [{
+            ctrl: "input",
+            options: choices,
+            variable: "__choice__"
+        }];
+        for (let i = 0; i < choices.length - 1; i++) {
+            const offset = this.branchOffsets[i];
+            if (offset) {
+                output.push({
+                    ctrl: "branch",
+                    op: "<=",
+                    v1: "__choice__",
+                    v2: i,
+                    offset: offset - i - 1
+                });
+            } else {
+                output.push({ ctrl: 'nop' });
+                console.warn("Choice in choice branch has no offset; will flow-over");
+            }
+        }
+        if (choices.length > 0) {
+            const offset = this.branchOffsets[choices.length - 1];
+            if (offset) {
+                output.push({
+                    ctrl: "jump",
+                    offset: offset - choices.length
+                });
+            } else {
+                output.push({ ctrl: 'nop' });
+                console.warn("Final choice in branch has no offset; will flow-over");
+            }
+        } else {
+            console.warn("Choice branch has no branch offsets");
+        }
+        return output;
+    }
+
+    private getChoices(): string[] {
+        const choices = [];
+        for (const line of this.choiceLines) {
+            choices.push(line.editable.getValue());
+        }
+        return choices;
+    }
+}
+
+class ChoiceBranchMacroLineOpening extends InstructionLine {
+    constructor() {
+        super();
+        this.setAreas("Choice branch", this.createEditable(""));
+        this.elm.class("jump");
+    }
+}
+class ChoiceBranchMacroLineOption extends BranchInstructionLine {
+    public editable: Editable;
+
+    constructor(choice: string) {
+        super();
+        this.setAreas("-> ", this.editable = this.createEditable(choice));
+        this.elm.class("jump");
     }
 }
