004d1e7cd3e6d43289bc284b6eb25b4bfe6f1a17
JaPNaA <japnaa@japnaa.dev>
2023-07-20 19:57:18 -0400

    Better match markdown standards

--- src/plugins/visualNovel/visualNovelMd.ts
+++ src/plugins/visualNovel/visualNovelMd.ts
@@ -1,17 +1,17 @@
 const headingRegex = /^\s*(#{1,6})\s*(.+)$/mg;
-const underscoreBoldRegex = /__([^\s][^]+?[^\s])__/g;
-const asteriskBoldRegex = /\*\*([^\s][^]+?[^\s])\*\*/g;
-const underscoreItalicRegex = /_([^\s][^]+?[^\s])_/g;
-const asteriskItalicRegex = /\*([^\s][^]+?[^\s])\*/g;
+const underscoreBoldRegex = /(^|\W)__([^\s](?:[^]*?[^\s])??)__(?=$|\W)/g;
+const asteriskBoldRegex = /\*\*([^\s*](?:[^]*?[^\s*])?)\*\*/g;
+const underscoreItalicRegex = /(^|\W)_([^\s](?:[^]*?[^\s])??)_(?=$|\W)/g;
+const asteriskItalicRegex = /\*([^\s*](?:[^]*?[^\s*])?)\*/g;
 const tagRegex = /&lt;([^<>&;]+?)&gt;([^]+?)&lt;\/\1&gt;/g;
 const variableRegex = /{(.+?)}/g;
 
 export function visualNovelMdToHTML(richText: string, getVariable?: (v: string) => string | undefined) {
     const preVariableText = _sanitizeHTML(richText)
         .replace(headingRegex, '<span class="volume" level="$1">$2</span>')
-        .replace(underscoreBoldRegex, '<b>$1</b>')
+        .replace(underscoreBoldRegex, '$1<b>$2</b>')
         .replace(asteriskBoldRegex, '<b>$1</b>')
-        .replace(underscoreItalicRegex, '<i>$1</i>')
+        .replace(underscoreItalicRegex, '$1<i>$2</i>')
         .replace(asteriskItalicRegex, '<i>$1</i>')
         .replace(tagRegex, '<span class="tag" tag="$1">$2</span>')
     if (getVariable) {
@@ -28,7 +28,7 @@ export function replaceVariables(text: string, getVariable: (v: string) => strin
 }
 
 function wrapInVisualNovelMd(text: string) {
-    return '<div class="visualNovelMD">' + text + "</div>";
+    return '<span class="visualNovelMD">' + text + "</span>";
 }
 
 function _sanitizeHTML(text: string): string {
