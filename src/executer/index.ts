8a63c0930949feee7fcf7f03312dfafaddcb55b8
JaPNaA <japnaa@japnaa.dev>
2023-08-02 16:48:09 -0400

    Create separate executer page

--- src/executer/index.ts
+++ src/executer/index.ts
@@ -1,3 +1,32 @@
-document.body.append("Executer");
+import { VisualNovelExecuter } from "../plugins/visualNovel/executer.js";
+import { Executer } from "./Executer.js";
+
+document.body.classList.add("executerContainer");
+
+const executer = new Executer({
+    startExecution(): Promise<void> {
+        return visualNovelExecuter.start(executerContainer);
+    },
+
+    runInstruction(instruction: any) {
+        return visualNovelExecuter.run(instruction);
+    },
+
+    stopExecution() {
+        return visualNovelExecuter.stop();
+    },
+
+    getExecutionStates() {
+        return { ["visualNovelPlugin.name"]: visualNovelExecuter.getState() };
+    },
+
+    setExecutionStates(states: { [x: string]: any }) {
+        visualNovelExecuter.setState(states["visualNovelPlugin.name"]);
+    }
+}).appendTo(document.body);
+
+const visualNovelExecuter = new VisualNovelExecuter();
+
+fetch("/testingFiles/exportTest/data/flows/0.json").then(data => data.json()).then(json => executer.execute(json));
 
 document.body.removeChild(document.getElementById("noLoadError")!);
