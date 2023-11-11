569b7a84dfee37508ca897e4d8cc300ee25dc5b3
JaPNaA <japnaa@japnaa.dev>
2023-11-11 01:47:26 -0500

    Add the import button to import JSON flows

--- src/editor/executer/ExecuterContainer.ts
+++ src/editor/executer/ExecuterContainer.ts
@@ -2,6 +2,7 @@ import { Executer } from "../../executer/Executer";
 import { Exporter } from "../../exporter/Exporter";
 import { FileStructureReadWrite } from "../../filesystem/FileStructure";
 import { Component, Elm } from "../../japnaaEngine2d/elements";
+import { constructInstructionData } from "../editor/toolchain/flowToInstructionData";
 import { appHooks, pluginHooks } from "../index";
 import { Project } from "../project/Project";
 import { download, requestFile } from "../utils";
@@ -16,35 +17,57 @@ export class ExecuterContainer extends Component {
 
         this.elm.append(
             new Elm().class("fileOperationsBar", "operationsBar").append(
-                new Elm("button").append("Run").onActivate(() => this.execute()),
-                new Elm("button").append("Save").onActivate(() =>
-                    appHooks.saveEditor()
-                    // download(
-                    //     stringToBlob(JSON.stringify(appHooks.getEditorSaveData())),
-                    //     "flowEditorSave.json"
-                    // )
-                ),
-                new Elm("button").append("Load").onActivate(() =>
-                    requestFile()
-                        .then(file => file.text())
-                        .then(text => appHooks.setEditorSaveData(JSON.parse(text)))
-                ),
-                new Elm("button").append("TextOp").onActivate(() => {
-                    appHooks.requestEditorTextOp();
-                }),
-                new Elm("button").append("Export").onActivate(async () => {
-                    const project = this.getProject() as FileStructureReadWrite;
-                    const startFlowFile = JSON.stringify({ flow: appHooks.getCompiledFlowFromEditor() });
+                new Elm("button").append("Run")
+                    .attribute("title", "Run the flow in the editor")
+                    .onActivate(() => this.execute()),
+                new Elm("button").append("Save")
+                    .attribute("title", "Save the current flow in the editor")
+                    .onActivate(() =>
+                        appHooks.saveEditor()
+                        // download(
+                        //     stringToBlob(JSON.stringify(appHooks.getEditorSaveData())),
+                        //     "flowEditorSave.json"
+                        // )
+                    ),
+                new Elm("button").append("Load")
+                    .attribute("title", "Load from a Flow Editor save file")
+                    .onActivate(() =>
+                        requestFile()
+                            .then(file => file.text())
+                            .then(text => appHooks.setEditorSaveData(JSON.parse(text)))
+                    ),
+                new Elm("button").append("TextOp")
+                    .attribute("title", "Perform an operation on all text")
+                    .onActivate(() => {
+                        appHooks.requestEditorTextOp();
+                    }),
+                new Elm("button").append("Export")
+                    .attribute("title", "Export to executable HTML")
+                    .onActivate(async () => {
+                        const project = this.getProject() as FileStructureReadWrite;
+                        const startFlowFile = JSON.stringify({ flow: appHooks.getCompiledFlowFromEditor() });
 
-                    await project.writeFlow(project.getStartFlowPath(), startFlowFile);
+                        await project.writeFlow(project.getStartFlowPath(), startFlowFile);
 
-                    download(await new Exporter(project).exportToSingleHTML(), "export.html");
-                }),
-                new Elm("button").append("Delete all").class("deleteAndReload").onActivate(() => {
-                    if (confirm("Delete editor contents and reload?")) {
-                        appHooks.setEditorSaveData(null);
-                    }
-                })
+                        download(await new Exporter(project).exportToSingleHTML(), "export.html");
+                    }),
+                new Elm("button").append("Import")
+                    .attribute("title", "Import an exported flow JSON file")
+                    .onActivate(async () => {
+                        requestFile()
+                            .then(file => file.text())
+                            .then(text => appHooks.setEditorSaveData({
+                                startGroup: 0,
+                                elms: constructInstructionData(JSON.parse(text))
+                            }));
+                    }),
+                new Elm("button").append("Delete all")
+                    .attribute("title", "Delete everything in the editor")
+                    .class("deleteAndReload").onActivate(() => {
+                        if (confirm("Delete editor contents and reload?")) {
+                            appHooks.setEditorSaveData(null);
+                        }
+                    })
             ),
             this.executer
         );
