2dc9aab3674f181581fbea3ab9c48cac7f2793b2
JaPNaA <japnaa@japnaa.dev>
2024-05-19 17:03:18 -0400

    Add documentation to FS

--- src/filesystem/FS.ts
+++ src/filesystem/FS.ts
@@ -1,22 +1,103 @@
-/** (My) FileSystem, readonly */
+/**
+ * (My) FileSystem, readonly.
+ * 
+ * Note that all paths do not end in a trailing connector. (For example,
+ * 'path/to/directory' is allowed, but 'path/to/directory/' is not.)
+ */
 export interface FSRead {
+    /**
+     * The name is usually the base of the directory. For example, if the object
+     * is based on "/home/username/Documents", then the name is "/home/username/Documents"
+     */
     name: string;
+
+    /**
+     * Read a file at the path.
+     * 
+     * @param path Path from base
+     */
     read(path: string): Promise<Blob>;
+
+    /**
+     * Change the root directory for this object.
+     * 
+     * For example, `cd(path).read(name)` is equivalent to `read(join(path, name))`
+     * 
+     * @param path Path to rebase to
+     */
     cd(path: string): Promise<FSRead>;
+
+    /**
+     * Join paths such that for `paths = [path1, path2, ...]`,
+     * `cd(path1).cd(path2). ...` is equivalent to `cd(join(paths))`
+     * 
+     * In most file systems, this is `path1 + '/' + path2 + ...`
+     * 
+     * @param paths List of path parts
+     */
     join(...paths: string[]): string;
 }
 
-/** (My) FileSystem, read and write */
+/**
+ * (My) FileSystem, read and write
+ * 
+ * Note that all paths do not end in a trailing connector. (For example,
+ * 'path/to/directory' is allowed, but 'path/to/directory/' is not.)
+ */
 export interface FSReadWrite extends FSRead {
     cd(path: string,): Promise<FSReadWrite>;
 
-    // is also 'readonly', but should not be required for executing flows
+    /**
+     * Retrieve the last modified unix (millisecond) timestamp of a file. This
+     * information may not be available in some file systems, so the function
+     * may return `null`.
+     * 
+     * lastModified is required for FSReadWrite but not FSRead since there
+     * is no use for lastModified in read-only fileSystems.
+     * 
+     * @param path Path to file
+     */
     lastModified(path: string): Promise<number | null>;
+
+    /**
+     * Get a list of files and directories under a directory.
+     * @param dir Path to directory.
+     */
     ls(dir: string): Promise<FSReference[]>;
 
+    /**
+     * Write content to some file. Will overwrite existing files in the same
+     * path and also create parent directories if needed.
+     * @param path Path of file
+     * @param blob Content of file to write
+     */
     write(path: string, blob: Blob): Promise<void>;
+
+    /**
+     * Moves an item (file or directory) from an original path to a new path.
+     * 
+     * Will overwrite files or directories on the destination path.
+     * 
+     * Note: `mv(file, directory)` will delete the directory and replace it
+     * with the file.
+     * 
+     * @param from Original item location
+     * @param to New item location
+     */
     mv(from: string, to: string): Promise<void>;
+
+    /**
+     * Removes an item (file or directory) at the specified path.
+     * Will recursively remove directories and directory contents.
+     * @param path Path of item
+     */
     rm(path: string): Promise<void>;
+
+    /**
+     * Create an empty directory at the specified path. Will overwrite files
+     * at the same path.
+     * @param path Path to directory.
+     */
     mkdir(path: string): Promise<void>;
 }
 
